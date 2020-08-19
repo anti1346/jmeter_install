239b24b7a60eeace2afafe6aeaa5835f2d76ec60
anti1346@gmail.com <42926329+anti1346@users.noreply.github.com>
2020-08-19 17:03:03 +0900

    Update jmeter_install.sh

--- jmeter_install.sh
+++ jmeter_install.sh
@@ -13,3 +13,9 @@ export JMETER_HOME=/usr/local/jmeter
 export PATH=$JMETER_HOME/bin:$PATH
 
 jmeter -v
+
+##부하 테스트 명령어
+#jmeter -n -f -t www_testplan.jmx -l www_log.jtl -e -o www-report
+
+##결과 웹페이지
+#docker run -d -v /usr/local/jmeter/bin/www-report:/usr/share/nginx/html -p 80:80 --name nginx nginx
